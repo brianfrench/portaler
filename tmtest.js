bad72995e236498e524a5ad72277282143487a58
brianfrench <brianfrench@users.noreply.github.com>
2015-02-25 08:54:39 -0500

    Update tmtest.js

--- tmtest.js
+++ tmtest.js
@@ -50,6 +50,10 @@ var GF=function(){
     this.lerp=function(a, b, t){
         return this.add(this.mul(a, 1 - t), this.mul(b, t));
     };
+    this.abcd_adcb=function(a, b, c, d){
+        return this.dot(a, b)*this.dot(c, d) -
+               this.dot(a, d)*this.dot(c, b);
+    };
     
     this.pclip=function(poly, p, n){
         // if(n===[[0,0,0]]){return [];}
@@ -113,16 +117,16 @@ var GF=function(){
     };
 };
 
-var rq=[1, 0, 0, 0], vo=[1.1, 1.1, 1.1], vdo=[0.1, 0, 0], vhi=0;
-rq=[2,1,1,1];
-var mapon=1;
-
+var rq=[1, 0, 0, 0], rdq=[0,0,0,0], vo=[0,0,3], vdo=[0.1, 0, 0], vhi=0;
+rq=[3,-1,-1,1];
+var pause=0, lighta=1;
 
 
 var kp=[], keyPressed=function(){
     kp[keyCode]=1;
     // if(keyCode===SHIFT){gtex.length=0;ntex=[0,6,0,0];}
-    if(keyCode==='M'.charCodeAt(0)){mapon^=1;}
+    if(keyCode==='P'.charCodeAt(0)){pause^=1;loop();}
+    if(keyCode==='L'.charCodeAt(0)){lighta^=1;}
     
 },keyReleased=function(){kp[keyCode]=0;};
 for(var i=0; i<256; i++){kp[i]=0;}
@@ -140,53 +144,70 @@ var phys=function(){
     //     [1, -(mouseY - height/2)*1e-4, (mouseX - width/2)*1e-4, 0]));
     // vdo=g.mul(g.add(vdo, g.mul(g.qrot(rq, [0,0,1]), 
     //     (mouseIsPressed?0.01:0)*(mouseButton===RIGHT?-1:1))), 0.9);
+    var rm=dt*5e1;
     rq=g.norm(g.qm(rq,
-        [1, (kp[UP] - kp[DOWN])*2e-2, (kp[RIGHT] - kp[LEFT])*2e-2, 0]));
+        g.norm([cos(rm), sin(rm)*(kp[UP] - kp[DOWN]),
+                         sin(rm)*(kp[RIGHT] - kp[LEFT]), 0])));
     
     vdo=g.mul(vdo, pow(0.1, dt));
-    vdo=g.add(vdo, g.mul([1,0,0],//g.qrot(rq, [0,0,1]), 
+    vdo=g.add(vdo, g.mul(g.qrot(rq, [0,0,1]),//[1,0,0],
         (kp['W'.charCodeAt(0)] - kp['S'.charCodeAt(0)])*10*dt));
-    vdo=g.add(vdo, g.mul([0,1,0],//g.qrot(rq, [1,0,0]), 
+    vdo=g.add(vdo, g.mul(g.qrot(rq, [1,0,0]),//[0,1,0],
         (kp['D'.charCodeAt(0)] - kp['A'.charCodeAt(0)])*10*dt));
-    vdo=g.add(vdo, g.mul([0,0,1],//g.qrot(rq, [0,1,0]), 
+    vdo=g.add(vdo, g.mul([0,0,-1],//g.qrot(rq, [0,1,0]), 
         (kp['Q'.charCodeAt(0)] - kp['E'.charCodeAt(0)])*10*dt));
     
-    vdo=g.add(vdo, [0,sin(ms*2e-1)*4e-3,0]);
+    vdo=g.add(vdo, [0,0,sin(ms*2e-1)*8e-3]);
     vo=g.add(vo, g.mul(vdo, dt));
+    
+    if(vo[2]<0){
+        vo[2]=0;
+        vdo[2]=abs(vdo[2]);
+    }
 };
 
-var rscr=function(p,wid,hei, pt, po, tex, bri){
+var rsc=function(face, view, tex, bri, screen){
     var g=new this.GF();
-    var vo=po[0], vf=po[1], vr=po[2], vd=po[3];
-    var vu=pt[2], vv=pt[3], vn=pt[1], vt=pt[0];
-    var ve=g.sub(po[0], vt);
-    var siz=198, isiz=1/siz;
-    // var h=~~0x495f3210;
-    for(var y=-siz; y<siz; y++){
-        var vb=g.add(vf, g.mul(vd, y*isiz));
-        var b=g.dot(vn, vr)*isiz, a=g.dot(vn, vb) - siz*b;
-        var d=(g.dot(ve, vu)*g.dot(vn, vr) + g.dot(vr, vu)*g.dot(vn, ve))*isiz, c=g.dot(ve, vu)*g.dot(vn, vb) + g.dot(vb, vu)*g.dot(vn, ve) - siz*d;
-        var f=(g.dot(ve, vv)*g.dot(vn, vr) + g.dot(vr, vv)*g.dot(vn, ve))*isiz, e=g.dot(ve, vv)*g.dot(vn, vb) + g.dot(vb, vv)*g.dot(vn, ve) - siz*f;
-        var l=80200 + y*wid - siz << 2, le=l + (siz << 3);
-        for(; l<le; l+=4, a+=b, c+=d, e+=f){
-          var m=1/a, u=c*m|0, v=e*m|0, ua=u&63, va=v&63;
-        //   ua++;va++;
-          var h=bri[(u>>6&3) | (v>>4&12)];
-        //   var j=(h << 8 & 0x3f000) + va*(h << 15 >> 19) +
-        //         ua*((h << 8 >> 19) + va*(h >> 24));
-          var j=(h << 12 & 0x7f000) + va*(h << 17 >> 18) +
-                ua*((h << 9 >> 18) + va*(h >> 23));
-        //   var j=0x40000;
-        //   var i=((u ^ v)&15) + 214;
-          var i=tex[ua | va << 6];
-          p[l  ]=j*(i >> 16) >> 19;
-          p[l+1]=j*(i >> 8 & 255) >> 19;
-          p[l+2]=j*(i & 255) >> 19;
+    var p=screen.p, ppl=screen.ppl;
+    
+    var od=g.sub(view.o, face.o);
+    var zbb=g.dot(view.c, face.n),
+        zbm=g.dot(view.d, face.n),
+        zm= g.dot(view.r, face.n),
+        
+        ubb=g.abcd_adcb(od, face.u, view.c, face.n),
+        ubm=g.abcd_adcb(od, face.u, view.d, face.n),
+        um= g.abcd_adcb(od, face.u, view.r, face.n),
+        
+        vbb=g.abcd_adcb(od, face.v, view.c, face.n),
+        vbm=g.abcd_adcb(od, face.v, view.d, face.n),
+        vm= g.abcd_adcb(od, face.v, view.r, face.n);
+    
+    for(var y=0; y<screen.h; y++){
+        var l=y*ppl << 2, el=l + (ppl << 2);
+        var zb=zbb + y*zbm, ub=ubb + y*ubm, vb=vbb + y*vbm;
+        for(; l<el; l+=4, zb+=zm, ub+=um, vb+=vm){
+            var z=1/zb, u=z*ub|0, v=z*vb|0, ua=u&31, va=v&31;
+            var h=bri[(u>>5&15) | (v>>1&240)];
+            var b=(h << 12 & 0x7f000) + va*(h << 17 >> 17) +
+                  ua*((h << 9 >> 17) + va*(h >> 21));
+            // var b=(h << 12 & 0x7f000) + va*(h << 17 >> 23 << 6) +
+            //       ua*((h << 9 >> 23 << 6) + va*(h >> 21));
+            var t=tex[(u&63) | (v&63) << 6];
+            // var t=0xffffff;
+            p[l  ]=b*(t >> 16) >> 19;
+            p[l+1]=b*(t >> 8 & 255) >> 19;
+            p[l+2]=b*(t & 255) >> 19;
         }
     }
 };
 
+
+
 var gtex=[], ntex=[0,6,0,0], texf=function(x,y){
+    if(1){
+        // return[1,1,1];
+    }
     var g=new GF();//this.g;
     var s=20;
     var u,v,m,n,w, m0,rn0,vx=0,vy=0;
@@ -212,6 +233,7 @@ var gtex=[], ntex=[0,6,0,0], texf=function(x,y){
         h=(sqrt(d1) - sqrt(d0))*24;
         if(h>1){h=1;}
         if(h<0){h=0;}
+        // h*=h;
         h*=h*(3 - 2*h);
         var w=rn0*1e-8;
         m=noise(d0x, d0y, w);
@@ -235,14 +257,88 @@ var gtex=[], ntex=[0,6,0,0], texf=function(x,y){
     // return [x,y,0.5];
 };
 
+var gbri=[];
+
 mouseClicked=function(){
     if(mouseButton===RIGHT){
         ntex=[0,6,0,0];
+    }else{
+        // gbri.length=0;
     }
 };
 
+var mbri=function(lp, ld){
+    var g=new this.GF();
+    var br=[];
+    // var lp=[0.1,0.1,1], ld=g.norm([1,1,-3]);
+    // lp=[sin(t*50)*0.6 + 0.5,
+    //     cos(t*40)*0.6 + 0.5,
+    //     0.3 + sin(t*20)*0.2];
+    
+    // lp=[0.5,0.5,0.2];
+    
+    for(var i=0; i<256; i++){
+        var x=(i&15)/16, y=(i>>4)/16;
+        
+        var n=0;
+        var d=g.sub([x,y,0], lp);
+        var m=g.dot(g.norm(d), ld);
+        // m=(m*5-4.6);
+        // m=(m*20 - 19);
+        // m=m*0.2 - 0.13;
+        // m=pow
+        // m-=0.9;
+        m=m*10 - 9;
+        if(m<0){m=0;}
+        m=m*0.5 + 1*this.pow(1-4*this.pow(m-0.5,2),10) +
+            2*this.pow(m,16);
+        // m*=m;
+        m*=0.2/g.mag2(d);
+        m=m>1?1:m<0?0:m;
+        n+=m;
+        // }
+        n=n>1?1:n<0?0:n;
+        br[i]=0.95*(n - 1) + 1;
+        
+    }
+    for(var j=0; j<1; j++){
+        var cr=br.slice();
+        for(var i=0; i<256; i++){
+            var x=i&15, y=i&240;
+            // br[i]=(cr[i]*2 +
+            //         cr[(x+1&15)|y] +
+            //         cr[x|(y+16&240)] +
+            //         cr[(x-1&15)|y] +
+            //         cr[x|(y-16&240)])/6;
+            var m=0;
+            for(var k=0; k<9; k++){
+                m+=cr[(x+k%3&15)|(y+(k/3|0)*16&240)]*
+                    // [53,97,53, 97,100,97, 53,97,53][k];
+                    [9,46,9, 46,100,46, 9,46,9][k];
+            }
+            br[i]=m/320;
+        }
+    }
+    for(var i=0; i<256; i++){br[i]=this.pow(br[i], 0.45)*127.9|0;}
+    var bri=[];
+    for(var i=0; i<256; i++){
+        // var 
+        var x=i&15, y=i&240,
+            a=br[i], b=br[(x+1&15)|y],
+            c=br[x|(y+16&240)], d=br[(x+1&15)|(y+16&240)];
+        var h=(a - b - c + d & 0x1ff) << 23 |
+              (b - a & 0xff) << 15 |
+              (c - a & 0xff) << 7 |
+              a;
+        bri[i]=h;
+    }
+    //gbri=bri.slice();
+    // cos(0);
+    return bri;
+};
 
 draw= function() {
+    if(pause){noLoop();return;}
     if(gtex.length<0x4000){
         for(var i=0; i<0x4000; i++){
             gtex[i]=(random(16777216)&0x3f3f3f)+0xa0a0a0;
@@ -263,11 +359,8 @@ draw= function() {
     
     while(ntex[1]>=0){
         if(((ntex[0]&7)===0||ntex[1]>4)&&millis()-ms>6){break;}
-        // gtex[ntex]=texf((ntex&127)/128, (ntex>>7)/128);
         var s=1<<ntex[1];
         if(((ntex[2]|ntex[3])&s) || ntex[1]===6){
-            // var vc=texf(ntex[2]/64, ntex[3]/64);
-            
             var vc=[0,0,0], x=ntex[2]/64, y=ntex[3]/64;
             for(var i=0; i<4; i++){
                 vc=g.add(vc, texf(x+(i&1)/128, y+(i>>1)/128));
@@ -304,43 +397,43 @@ draw= function() {
         vd=g.qrot(rq, [0,1,0]);
     
     if(!loadPixels){return;}
-    if(!this.imageData||!this.imageData.data||0){
-        background(100);
-        loadPixels();}
-    if(!this.imageData||!this.imageData.data){return;}
-    // var p=this.imageData.data;
+    if(!this.imageData || !this.imageData.data || 0){
+        background(0xff552255); loadPixels();}
+    if(!this.imageData || !this.imageData.data){return;}
     
-    var vu=[100,0,0], vv=[0,100,0], vn=[0,0,1], vt=[0,0,5];
+    var vu=[100,0,0], vv=[0,100,0], vn=[0,0,1], vt=[0,0,0];
     
-    
-    var br=[];
-    for(var i=0; i<16; i++){
-        var m=noise((i&3)*0.02 - t*0.05, (i>>2)*0.02 + t*0.05, t*0.02);
-        // for(var j=0; j<4; j++){m*=m*(3 - 2*m);}
-        // var m=noise((i&3)*0.2, (i>>2)*0.2, t*0.2);
-        m=sin(m*1e4)*0.5 + 0.5;
-        m=sqrt(m*0.8 + 0.2);
-        m=m*128 + 0|0;
-        br[i]=m;
-        // br[i]=m>0.999?0.999:m;
-    }
-    // br[frameCount>>4&15]=0;
-    var bri=[];
-    for(var i=0; i<16; i++){
-        // var 
-        var x=i&3, y=i&12,
-            a=br[i], b=br[(x+1&3)|y],
-            c=br[x|(y+4&12)], d=br[(x+1&3)|(y+4&12)];
-        var h=(a - b - c + d & 0x1ff) << 23 |
-              (b - a & 0xff) << 15 |
-              (c - a & 0xff) << 7 |
-              a;
-        bri[i]=h;
+    if(!gbri.length||lighta){
+        var m=mouseX*0.5 + t*30;
+        var lp=[0.5*(sin(m)*0.7 + sin(m*3)*0.5 + 1),
+                0.5*(cos(m)*0.7 - cos(m*3)*0.5 + 1),
+                0.4];
+                // 0.1 + (1 - 4*sq(t*0.5%1 - 0.5))*0.2];
+        // var lp=[0.5*(sin(t*50)*0.7 + sin(t*150)*0.5 + 1),
+        //         0.5*(cos(t*50)*0.7 - cos(t*150)*0.5 + 1),
+        //         0.3];
+        // var ld=g.norm(g.sub([mouseX/width,mouseY/height,0.0], lp));
+        var ld=g.norm(g.sub([0.5,0.5,lp[2]-0.3], lp));
+        ld=g.qrot([cos(sin(m*4)*10), 0, 0, sin(sin(m*4)*10)], ld);
+        // var ld=g.norm(g.sub([0.5,0.5,0], lp));
+        // var ld=[0,0,-1];
+        
+        gbri=mbri(lp, ld);
     }
     
-    rscr(this.imageData.data, width, height,
-        [vt,vn,vu,vv], [vo,vf,vr,vd],
-        gtex, bri);
+    // rscr(this.imageData.data, width, height,
+    //     [vt,vn,vu,vv], [vo,vf,vr,vd],
+    //     gtex, bri);
+        
+    rsc({p:[[]], o:vt, n:vn, u:vu, v:vv},
+        {   o:vo,//[1,1,-3],
+            c:g.sub(vf, g.add(g.mul(vr, width/height), vd)),
+            r:g.mul(vr, 2/height),
+            d:g.mul(vd, 2/height)},
+        gtex,
+        gbri,
+        {p:this.imageData.data,
+            x:0, y:0, w:width, h:height, ppl:width});
     
     updatePixels();
     
@@ -348,6 +441,7 @@ draw= function() {
     fill(250, 3, 3);
     for(var i=0, s=''; i<3; i++){s+=vo[i].toFixed(2)+"\n";}
     
-    text(millis()-ms+"\n"+(this.__frameRate).toFixed(1)+"\n\n"+s+"\n"/*+vd[0].p*/, 4,180,380,380);
+    text(millis()-ms+"\n"+(this.__frameRate).toFixed(1)+"\n\n"+s+"\n"/*+vd[0].p*/, 4,16);
 };
 textSize(14);
+
